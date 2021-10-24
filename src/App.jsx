b41ebbbf70bc60585d2ed4b35d2b2b32d1409eb0
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-10-24 12:33:26 +0530

    feat: added home, login, admin pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+//components
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/admin">
+            <Admin />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }

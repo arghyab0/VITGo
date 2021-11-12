0586c4ff65dd298218b802683f0f6660ea3f65ab
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-11-12 20:06:08 +0530

    feat: added outing history comp

--- src/pages/Requests.jsx
+++ src/pages/Requests.jsx
@@ -1,12 +1,13 @@
 //components
 import { Row, Col, Container } from "react-bootstrap";
 import RequestForm from "../components/RequestForm";
+import OutingHistory from "../components/OutingHistory";
 
 const Requests = () => {
   return (
     <>
       <Row>
-        <Col md={6} className="mt-5" style={{ border: "1px black solid" }}>
+        <Col md={6} className="mt-5">
           <Container>
             <h3 className="d-flex justify-content-center mb-4">
               New outing request
@@ -14,11 +15,12 @@ const Requests = () => {
             <RequestForm />
           </Container>
         </Col>
-        <Col md={6} className="mt-5" style={{ border: "1px black solid" }}>
+        <Col md={6} className="mt-5">
           <Container>
             <h3 className="d-flex justify-content-center mb-4">
               Outing request history
             </h3>
+            <OutingHistory />
           </Container>
         </Col>
       </Row>

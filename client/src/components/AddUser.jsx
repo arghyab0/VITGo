356b3c839d0e52a456944575f33228adf9c3d596
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-11-14 22:14:35 +0530

    feat: added handlers

--- client/src/components/AddUser.jsx
+++ client/src/components/AddUser.jsx
@@ -4,17 +4,49 @@ import { Row, Col, Form, Button } from "react-bootstrap";
 import ConfirmationModal from "./ConfirmationModal";
 
 const AddUser = () => {
+  const [userID, setUserID] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [type, setType] = useState("");
   const [modalShow, setModalShow] = useState(false);
+
+  // console.log(userID, name, email, password, type);
+
+  const addUser = (e) => {
+    e.preventDefault();
+
+    // setModalShow(true);
+  };
 
   return (
     <>
-      <Form>
+      <Form onSubmit={addUser}>
         <Form.Group as={Row} className="mb-5">
           <Form.Label column xs={4}>
             User ID
           </Form.Label>
           <Col xs={8}>
-            <Form.Control type="text" placeholder="Student/staff ID" />
+            <Form.Control
+              type="text"
+              placeholder="Student/staff ID"
+              value={userID}
+              onChange={(e) => setUserID(e.target.value)}
+            />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-5">
+          <Form.Label column xs={4}>
+            Display name
+          </Form.Label>
+          <Col xs={8}>
+            <Form.Control
+              type="text"
+              placeholder="Display name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </Col>
         </Form.Group>
 
@@ -23,7 +55,12 @@ const AddUser = () => {
             User email
           </Form.Label>
           <Col xs={8}>
-            <Form.Control type="email" placeholder="User email" />
+            <Form.Control
+              type="email"
+              placeholder="User email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </Col>
         </Form.Group>
 
@@ -32,7 +69,12 @@ const AddUser = () => {
             Password
           </Form.Label>
           <Col xs={8}>
-            <Form.Control type="password" placeholder="Deafult password" />
+            <Form.Control
+              type="password"
+              placeholder="Default password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </Col>
         </Form.Group>
 
@@ -41,7 +83,11 @@ const AddUser = () => {
             User type
           </Form.Label>
           <Col xs={8}>
-            <Form.Select defaultValue="Choose">
+            <Form.Select
+              defaultValue="Choose"
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+            >
               <option>Student</option>
               <option>Hostel manager</option>
               <option>Security personnel</option>
@@ -51,9 +97,7 @@ const AddUser = () => {
 
         <Form.Group as={Row}>
           <Col className="d-flex justify-content-center">
-            <Button type="submit" onClick={() => setModalShow(true)}>
-              Add user
-            </Button>
+            <Button type="submit">Add user</Button>
           </Col>
         </Form.Group>
       </Form>

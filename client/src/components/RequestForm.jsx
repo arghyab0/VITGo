d537802df8466777f03c926b9ddfe89a89f19f97
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-11-18 15:22:20 +0530

    feat: implemented raising request

--- client/src/components/RequestForm.jsx
+++ client/src/components/RequestForm.jsx
@@ -1,16 +1,51 @@
 //components
+import { useState } from "react";
 import { Row, Col, Form, Button } from "react-bootstrap";
+import axios from "axios";
 
 const RequestForm = () => {
+  const [contactNo, setContactNo] = useState("");
+  const [parentsContactNo, setParentsContactNo] = useState("");
+  const [hours, setHours] = useState("");
+  const [purpose, setPurpose] = useState("");
+
+  const resetFields = () => {
+    setContactNo("");
+    setParentsContactNo("");
+    setHours("");
+    setPurpose("");
+  };
+
+  const addRequest = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("/api/request/", {
+        // issuedBy: (pass user id from store here)
+        // issuedBy: "6193c32bc997194e926ea0f1", (was testing with hard coded val)
+        contactNo,
+      });
+      console.log(res.data);
+      resetFields();
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
+
   return (
     <>
-      <Form>
+      <Form onSubmit={addRequest}>
         <Form.Group as={Row} className="mb-3">
           <Form.Label column xs={3}>
             Contact no.
           </Form.Label>
           <Col xs={9}>
-            <Form.Control type="text" placeholder="Personal contact number" />
+            <Form.Control
+              type="text"
+              placeholder="Personal contact number"
+              value={contactNo}
+              onChange={(e) => setContactNo(e.target.value)}
+            />
           </Col>
         </Form.Group>
 
@@ -19,7 +54,12 @@ const RequestForm = () => {
             Parent's contact no.
           </Form.Label>
           <Col xs={9}>
-            <Form.Control type="text" placeholder="Parent's contact number" />
+            <Form.Control
+              type="text"
+              placeholder="Parent's contact number"
+              value={parentsContactNo}
+              onChange={(e) => setParentsContactNo(e.target.value)}
+            />
           </Col>
         </Form.Group>
 
@@ -28,7 +68,12 @@ const RequestForm = () => {
             Time (hrs.)
           </Form.Label>
           <Col xs={9}>
-            <Form.Control type="number" placeholder="Hours of leave" />
+            <Form.Control
+              type="number"
+              placeholder="Hours of leave"
+              value={hours}
+              onChange={(e) => setHours(e.target.value)}
+            />
           </Col>
         </Form.Group>
 
@@ -37,7 +82,12 @@ const RequestForm = () => {
             Purpose of outing
           </Form.Label>
           <Col xs={8}>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Control
+              as="textarea"
+              rows={3}
+              value={purpose}
+              onChange={(e) => setPurpose(e.target.value)}
+            />
           </Col>
         </Form.Group>
 

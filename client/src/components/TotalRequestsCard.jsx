fdfc12ba94f33bbfe35201e2be91c67d41aacf7b
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-11-25 23:03:45 +0530

    refactor: comp state fetching

--- client/src/components/TotalRequestsCard.jsx
+++ client/src/components/TotalRequestsCard.jsx
@@ -1,29 +1,13 @@
 //components
-import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
-import axios from "axios";
 
-const TotalRequestsCard = () => {
-  const [totalRequests, setTotalRequests] = useState(0);
-
-  useEffect(() => {
-    const fetchStatus = async () => {
-      const res = await axios.get("/api/request");
-      const myRequests = res.data.filter(
-        (item) => item.issuedBy === "6193b68bb2b336e4d2335211"
-      );
-
-      setTotalRequests(myRequests.length);
-    };
-    fetchStatus();
-  }, []);
-
+const TotalRequestsCard = ({ reqData }) => {
   return (
     <Card>
       <Card.Body>
         <Card.Title>Total requests</Card.Title>
-        <Card.Text>{totalRequests}</Card.Text>
+        <Card.Text>{reqData.length}</Card.Text>
         <Card.Link className="d-flex justify-content-center">
           <Link to="/requests">New request</Link>
         </Card.Link>

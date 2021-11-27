8d87f55ec3556450f9d0e9d37fd6a46d45cd2416
Arghya Biswas <arghyabiswas2000@gmail.com>
2021-11-27 19:36:41 +0530

    feat: deployment

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/request", requestRoute);
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running at port ${port}....`));
